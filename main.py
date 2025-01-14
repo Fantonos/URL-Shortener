b81e642086019108bee438898ea84d4cea0872fe
J <70118013+Fantonos@users.noreply.github.com>
2025-01-13 20:27:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import pyshorteners
 
-print("Main module is running")
+long_user_url = input("Enter the URL: ")
+type_tiny = pyshorteners.Shortener()
+short_user_url = type_tiny.tinyurl.short(long_user_url)
+
+print("The Shortened URL: " + short_user_url)
